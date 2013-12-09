d7cc0c9fd3e50eeecc4d2732c54bec5857e0adaa
jasonlap64 <jasonlap64@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-09 03:04:49 +0000

    Adding Plugin
    git-svn-id: https://plugins.svn.wordpress.org/iaw-mobile-redirect/trunk@817190 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ iaw-mobile-redirect.php
@@ -0,0 +1,121 @@
+<?php
+/*
+Plugin Name: IAW Mobile Redirect
+Description: Select a mobile web app to point mobile users to
+Author: LaPorte Consulting, LLC
+Version: 1.0.0
+Author URI: http://laporteconsult.com/
+*/
+
+/*	Copyright 2013 LaPorte Consulting, LLC (email : info@laporteconsult.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+*/
+
+$ios_mobile_redirect = new IAW_Mobile_Redirect();
+
+register_uninstall_hook( __FILE__, 'uninstall_iaw_mobile_redirect' );
+function uninstall_mobile_redirect() {
+	delete_option( 'iawmobileredirectapp' );
+	delete_option( 'iawmobileredirecttoggle' );
+	delete_option( 'iawmobileredirecttablet' );
+}
+
+class IAW_Mobile_Redirect{
+
+	function __construct() { //init function
+		add_action( 'admin_init', array( &$this, 'admin_init' ) );
+		add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
+		add_action( 'wp_head', array( &$this, 'iaw_mobile_redirect' ), 1 );
+		if ( get_option( 'iawmobileredirecttoggle' ) == 'true' )
+			update_option( 'iawmobileredirecttoggle', true );
+	}
+
+	function admin_init() {
+		add_filter( 'plugin_action_links_'. plugin_basename( __FILE__ ), array( &$this, 'plugin_action_links' ), 10, 4 );
+	}
+
+	function plugin_action_links( $actions, $plugin_file, $plugin_data, $context ) {
+		if ( is_plugin_active( $plugin_file ) )
+			$actions[] = '<a href="' . admin_url('options-general.php?page=iaw-mobile-redirect/iaw-mobile-redirect.php') . '">Configure</a>';
+		return $actions;
+	}
+
+	function admin_menu() {
+		add_submenu_page( 'options-general.php', __( 'IAW Mobile Redirect', 'iaw-mobile-redirect' ), __( 'IAW Mobile Redirect', 'iaw-mobile-redirect' ), 'administrator', __FILE__, array( &$this, 'page' ) );
+	}
+
+	function page() { //admin options page
+ 
+		//do stuff if form is submitted
+		if ( isset( $_POST['mobileapp'] ) ) {
+			update_option( 'iawmobileredirectapp', $_POST['mobileapp'] );
+			update_option( 'iawmobileredirecttoggle', isset( $_POST['mobiletoggle'] ) ? true : false );
+
+			update_option( 'iawmobileredirecttablet', isset( $_POST['mobileredirecttablet'] ) );
+
+			echo '<div class="updated"><p>' . __( 'Updated', 'iaw-mobile-redirect' ) . '</p></div>';
+		}
+
+		?>
+		<div class="wrap"><h2><?php _e( 'IAW Mobile Redirect', 'iaw-mobile-redirect' ); ?></h2>
+		<p>
+			<?php _e( 'If the checkbox is checked, and a valid Mobile App Code is inputted, this site will redirect to the specified Mobile Web App when visited by a mobile device.', 'iaw-mobile-redirect' ); ?>
+		</p>
+
+		<form method="post">
+		<p>
+			<label for="mobiletoggle"><?php _e( 'Enable Redirect:', 'iaw-mobile-redirect' ); ?>
+			<input type="checkbox" value="1" name="mobiletoggle" id="mobiletoggle" <?php checked( get_option('iawmobileredirecttoggle', ''), 1 ); ?> /></label>
+		</p>
+		<p>
+			<label for="mobileapp"><?php _e( 'Mobile App Code:', 'iaw-mobile-redirect' ); ?>
+			<input type="text" name="mobileapp" id="mobileapp" value="<?php echo get_option('iawmobileredirectapp', ''); ?>" /></label>
+		</p>
+		<p>
+			<label for="mobileredirecttablet"><?php _e( 'Redirect Tablets:', 'iaw-mobile-redirect' ); ?>
+			<input type="checkbox" value="1" name="mobileredirecttablet" id="mobileredirecttablet" <?php checked( get_option('iawmobileredirecttablet', ''), 1 ); ?> /></label>
+		</p>
+			<?php submit_button(); ?>
+		</form>
+		</div>
+
+<!--		<div class="copyFooter">Plugin written by <a href="http://laporteconsult.com">LaPorte Consulting, LLC</a>.</div>-->
+		<?php
+	}
+
+	function iaw_mobile_redirect() {
+		//check if tablet box is checked
+		if( get_option('iawmobileredirecttablet') == 0){
+			//redirect phone only
+			$script_name = "phone";
+		} else {
+			// redirect phone and tablet
+			$script_name = "phoneandtablet";
+		}
+
+		// not enabled
+		if ( ! get_option('iawmobileredirecttoggle') )
+			return;
+
+		$app_code = get_option('iawmobileredirectapp', '');
+		// empty url
+		if ( empty( $app_code ) )
+			return;
+?>
+<script language="javascript">var mobileId = "<?php echo $app_code ?>";</script> <script type="text/javascript" src="http://www.1tap.mobi/include/js/external/<?php echo $script_name ?>.js"></script>
+<?	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== IAW Mobile Redirect ===
+Contributors: jasonlap64
+Donate link: NA
+Tags: mobile, redirect, iaw
+Requires at least: 3.4
+Tested up to: 3.5
+Stable tag: 1.0.0
+License: GPLv2 or later 
+
+This plugin allows you to put in a mobile web app to redirect mobile traffic to.
+
+== Description ==
+
+A mobile redirect plugin for those who use a mobile web app for their mobile content.  Simply put in the Mobile App Code, Check the Enable Redirect and you are ready to send users to your mobile-only content.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `iaw-mobile-redirect.php` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Go to Settings > IAW Mobile Redirect.
+1. Check `Enable Redirect`.
+1. Put in a Mobile App Code the mobile web app you would like to redirect mobile traffic to.
+1. Hit `Save` and you are done.
+
+== Frequently Asked Questions ==
+
+= I've disabled the plugin but the redirect is still happening, how do I fix this? =
+
+First, check if this is happening on another computer. If not, the redirect is simply cached on your machine. Clear the cache and you should be fine.
+
+If the redirect remains everywhere, check if there are other redirect scripts running.
+
+= Does this redirect iPad traffic? =
+
+Yes, there is an option to redirect all tablet traffic.
+
+== Changelog ==
+
+= 1.0 =
+* Stable version
+
+== Screenshots ==
+1. The admin page
+
+== Upgrade Notice ==
+
+= 1.0 =
+No upgrades at this time.
